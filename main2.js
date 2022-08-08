51c03654419eb3af47752719b9d11922d45dce2c
siaohanghong99 <54624721+siaohanghong99@users.noreply.github.com>
2022-08-08 11:20:16 +0800

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', () => {
+
+  let liffID = '1657358806-qypVO7rz';
+    let appID = 'OAKS_LOGON_SSO';
+
+  triggerLIFF();
+
+  // 執行範例裡的所有功能
+  function triggerLIFF() {
+
+    liff.init({
+        liffId: liffID
+    }).then(() => {
+        let isInClient, isLoggedIn;
+
+        language = liff.getLanguage(); // String。引用 LIFF SDK 的頁面，頁面中的 lang 值
+        version = liff.getVersion(); // String。LIFF SDK 的版本
+        isInClient = liff.isInClient(); // Boolean。回傳是否由 LINE App 存取
+        isLoggedIn = liff.isLoggedIn(); // Boolean。使用者是否登入 LINE 帳號。true 時，可呼叫需要 Access Token 的 API
+        if (!isLoggedIn) {
+            alert('Not Login');
+        }else{
+            window.location.href = originUrl+'/index_line_auth.php?app_id='+encodeURIComponent(appID)+'&accessToken='+encodeURIComponent(liff.getAccessToken());
+        }
+    }).catch(error => {
+        console.log(error);
+    });
+
+  }
+
+})
